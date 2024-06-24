84ad2e12eecdd6aa039f9884d4000e00a0679844
FelipeYSU <125041064+FelipeUemura11@users.noreply.github.com>
2024-06-24 19:22:32 -0300

    Noticias/Discussoes(comunidade) - Enzo

--- ProjetoSTEAM2.0/Models/Comunidade.java
+++ ProjetoSTEAM2.0/Models/Comunidade.java
@@ -6,26 +6,57 @@ import java.util.Scanner;
 
 public class Comunidade {
     private List<Noticias> noticias;
+    private List<Discussao> discussoes;
 
     public Comunidade() {
         noticias = new ArrayList<>();
+        discussoes = new ArrayList<>();
         // Adicionando algumas Noticias de exemplo
-        noticias.add(new Noticias("Novo jogo de RPG lancado!", "Um novo RPG revolucionário acaba de ser lancado, trazendo graficos impressionantes e uma historia envolvente."));
-        noticias.add(new Noticias("Atualização massiva para jogo de estratégia!", "Uma nova atualização para o jogo de estratégia mais popular do ano foi lançada, trazendo novos mapas, unidades e modos de jogo."));
-        noticias.add(new Noticias("Entrevista com desenvolvedor de jogo indie!", "Conversamos com o desenvolvedor por trás do mais novo jogo indie aclamado pela crítica, discutindo o processo de criação e os desafios enfrentados."));
+        noticias.add(new Noticias("Tectoy revela linha Zeenix com PC portátil, periféricos e mais", "A Tectoy anunciou sua nova linha de produtos Zeenix, incluindo um PC portátil inovador e uma variedade de periféricos de alta performance."));
+        noticias.add(new Noticias("Elden Ring: Diminuir dificuldade 'quebraria o jogo', na opinião do criador", "O criador de Elden Ring afirmou que reduzir a dificuldade do jogo comprometeria a experiência pretendida, mantendo-se firme na decisão de manter o desafio."));
+        noticias.add(new Noticias("Resident Evil original pode ser lançado para PC em breve", "Rumores indicam que o clássico Resident Evil original está prestes a ser relançado para PC, trazendo nostalgia e terror para uma nova geração de jogadores."));
+        noticias.add(new Noticias("LoL receberá modo de jogo PvE em julho", "League of Legends está programado para receber um novo modo de jogo PvE em julho, oferecendo uma nova experiência de jogo para os fãs da série."));
+        
+        // Adicionando algumas Discussoes de exemplo
+        discussoes.add(new Discussao("Sucesso de ‘Palworld’ abre discussão sobre plágio nos games", "O sucesso de ‘Palworld’ tem levantado questões sobre plágio no mundo dos games. O que vocês acham?"));
+        discussoes.add(new Discussao("Jogos de Nintendo Switch finalmente sendo traduzidos? Vc concorda ou não", "Muitos jogos de Nintendo Switch estão sendo traduzidos para diversos idiomas. Vocês concordam com essa prática?"));
+        discussoes.add(new Discussao("Playstation, Xbox, Switch ou PC? Qual eu devo escolher?", "Estou pensando em comprar um novo console ou PC para jogos. Qual vocês recomendam e por quê?"));
+        discussoes.add(new Discussao("Qual é o melhor jogo de 2024 até agora na sua opinião?", "Até agora, qual jogo lançado em 2024 vocês consideram o melhor?"));
     }
 
     public void adicionarNoticia(Noticias noticia) {
         noticias.add(noticia);
     }
 
+    public void adicionarDiscussao(Discussao discussao) {
+        discussoes.add(discussao);
+    }
+
     public void exibirNoticias() {
+        if (noticias.isEmpty()) {
+            System.out.println("Nenhuma noticia disponivel.");
+            return;
+        }
 
         System.out.println("=============== Noticias ===============");
         for (Noticias noticia : noticias) {
             System.out.println("Titulo: " + noticia.getTitulo());
             System.out.println("Descricao: " + noticia.getConteudo());
-            System.out.println("----------------------------------");
+            System.out.println("--------------------------------------------------------------------------------------------");
+        }
+    }
+
+    public void exibirDiscussoes() {
+        if (discussoes.isEmpty()) {
+            System.out.println("Nenhuma discussao disponivel.");
+            return;
+        }
+
+        System.out.println("=============== Discussoes ===============");
+        for (Discussao discussao : discussoes) {
+            System.out.println("Titulo: " + discussao.getTitulo());
+            System.out.println("Descricao: " + discussao.getDescricao());
+            System.out.println("--------------------------------------------------------------------------------------------");
         }
     }
 
@@ -34,8 +65,9 @@ public class Comunidade {
         do {
             System.out.println("\n=============== Comunidade ===============");
             System.out.println("1. Ver Noticias");
-            // 2. Ver Discussoes ---> Comentarios de pessoas influentes na comunidade falando sobre jogos
-            System.out.println("3. Adicionar opiniao sobre Jogo");
+            System.out.println("2. Ver Discussoes");
+            System.out.println("3. Adicionar noticia de Jogo");
+            System.out.println("4. Adicionar Discussao");
             System.out.println("0. Voltar");
             System.out.print("Escolha uma opção: ");
 
@@ -47,14 +79,27 @@ public class Comunidade {
                     exibirNoticias();
                     break;
                 case 2:
-                    System.out.print("Digite o titulo da noticia: ");
-                    String titulo = scanner.nextLine();
-                    System.out.print("Digite o conteudo da noticia: ");
-                    String conteudo = scanner.nextLine();
+                    exibirDiscussoes();
+                    break;
+                case 3:
+                    System.out.print("Digite o título da noticia: ");
+                    String tituloNoticia = scanner.nextLine();
+                    System.out.print("Digite o conteúdo da noticia: ");
+                    String conteudoNoticia = scanner.nextLine();
 
-                    Noticias novaNoticia = new Noticias(titulo, conteudo);
+                    Noticias novaNoticia = new Noticias(tituloNoticia, conteudoNoticia);
                     adicionarNoticia(novaNoticia);
-                    System.out.println("Opiniao adicionada com sucesso!");
+                    System.out.println(" >> Notícia adicionada com sucesso! << ");
+                    break;
+                case 4:
+                    System.out.print("Digite o título da discussao: ");
+                    String tituloDiscussao = scanner.nextLine();
+                    System.out.print("Digite a descrição da discussao: ");
+                    String descricaoDiscussao = scanner.nextLine();
+
+                    Discussao novadiscussao = new Discussao(tituloDiscussao, descricaoDiscussao);
+                    adicionarDiscussao(novadiscussao);
+                    System.out.println(" >> Discussão adicionada com sucesso! << ");
                     break;
                 case 0:
                     break;
---
+++ ProjetoSTEAM2.0/Models/Discussao.java
@@ -0,0 +1,27 @@
+package Models;
+
+public class Discussao {
+    private String titulo;
+    private String descricao;
+
+    public Discussao(String titulo, String descricao) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
